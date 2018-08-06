54b74117b8cd81f7a2c6570a9ce83211400299d0
vita <dragic.hsieh@gmail.com>
2018-08-06 15:45:17 +0800

    Done add attributes to user model and modify zh-TW error message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_05_174931) do
+ActiveRecord::Schema.define(version: 2018_08_06_062039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 2018_08_05_174931) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "name"
+    t.string "avatar"
+    t.text "intro"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
